bc7671f95272f9f9e5de207f86284a9855e9abc5
KlimRazumof <35017602+KlimRazumof@users.noreply.github.com>
2019-01-11 22:51:06 +0300

    Передвижение персонажа по карте

---
+++ NetWorkGame/Assets/Scripts/PlayerScripts/UnitAnimation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitAnimation : MonoBehaviour
+{
+    private Animator _animator;
+    private UnitMotor _motor;
+
+    private int _walk;
+    private int _run;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _motor = GetComponent<UnitMotor>();
+
+        _walk = Animator.StringToHash("Walk");
+        _run = Animator.StringToHash("Run");
+    }
+
+    private void FixedUpdate()
+    {
+        MoveAnimation();
+    }
+
+    private void MoveAnimation()
+    {
+        if (_motor.Agent.hasPath)
+        {
+            if (_motor.IsWalk)
+                _animator.SetBool(_walk, true);
+            else
+                _animator.SetBool(_run, true);
+        }
+        else
+        {
+            _animator.SetBool(_walk, false);
+            _animator.SetBool(_run, false);
+        }
+    }
+}
---
+++ NetWorkGame/Assets/Scripts/PlayerScripts/UnitMotor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class UnitMotor : MonoBehaviour
+{
+    public bool IsWalk { get; set; }
+    public NavMeshAgent Agent { get; set; }
+
+    private void Start()
+    {
+        Agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void MoveToPoint(Vector3 point)
+    {
+        Agent.SetDestination(point);
+    }
+
+    public void SetSpeed(float speed)
+    {
+        Agent.speed = speed;
+    }
+}
